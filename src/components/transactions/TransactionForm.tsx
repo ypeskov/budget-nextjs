35c798927a8c378a66e4ee40f153ca48d64ba465
Yuriy Peskov <yuriy.peskov@gmail.com>
2025-01-26 18:44:21 +0200

    add support of different types of transactions including transfer

--- src/components/transactions/TransactionForm.tsx
+++ src/components/transactions/TransactionForm.tsx
@@ -22,12 +22,14 @@ type TransactionRequest = {
   accountId?: number;
   label: string;
   amount: number;
-  categoryId: number;
+  targetAccountId: number | null;
+  targetAmount: number | null;
+  categoryId: number | null;
   dateTime: string;
   notes: string;
   isTransfer: boolean;
   isIncome: boolean;
-}
+};
 
 const fetchAccounts = async () => {
   const authToken = getCookie('authToken');
@@ -37,17 +39,21 @@ const fetchAccounts = async () => {
     return accounts.json();
   }
   return [];
-}
+};
 
 const fetchCategories = async () => {
   const authToken = getCookie('authToken');
   const headers: HeadersInit = authToken ? { "auth-token": authToken } : {};
   const categories = await fetch(ApiRoutes.categories(), { headers });
   if (categories.ok) {
-    return categories.json();
+    const data: Category[] = await categories.json();
+    return {
+      expenseCategories: data.filter(category => category.isIncome === false),
+      incomeCategories: data.filter(category => category.isIncome === true),
+    };
   }
-  return [];
-}
+  return { expenseCategories: [], incomeCategories: [] };
+};
 
 const submitTransaction = async (transactionRequest: TransactionRequest, isNewTransaction: boolean): Promise<Transaction | null> => {
   const authToken = getCookie('authToken');
@@ -55,33 +61,47 @@ const submitTransaction = async (transactionRequest: TransactionRequest, isNewTr
 
   const method = isNewTransaction ? 'POST' : 'PUT';
   const response = await fetch(ApiRoutes.submitTransaction(), {
-    method: method,
+    method,
     headers: { "Content-Type": "application/json", ...headers },
-    body: JSON.stringify(transactionRequest)
+    body: JSON.stringify(transactionRequest),
   });
   if (response.ok) {
     return response.json();
   }
   console.error(response);
   return null;
-}
+};
 
 export const TransactionForm = ({ locale, closeForm, transaction }: TransactionFormProps) => {
   const t = useTranslations('');
   const [label, setLabel] = useState(transaction?.label || '');
   const [account, setAccount] = useState(transaction?.account.id || '');
   const [amount, setAmount] = useState(transaction?.amount || 0);
+  const [transactionType, setTransactionType] = useState(
+    transaction?.isTransfer ? 'transfer' : transaction?.isIncome ? 'income' : 'expense'
+  ); // "transfer", "income", "expense"
   const [category, setCategory] = useState(transaction?.category?.id || '');
-  const [date, setDate] = useState(transaction ? new Date(transaction.dateTime).toISOString().split('T')[0] : '');
-  const [time, setTime] = useState(transaction ? new Date(transaction.dateTime).toISOString().split('T')[1].split('.')[0] : '');
+  const [date, setDate] = useState(transaction ?
+    DateTime.fromISO(transaction.dateTime).toFormat("yyyy-MM-dd") :
+    DateTime.now().toFormat("yyyy-MM-dd"));
+  const [time, setTime] = useState(transaction ?
+    DateTime.fromISO(transaction.dateTime).toFormat("HH:mm") :
+    DateTime.now().toFormat("HH:mm"));
   const [notes, setNotes] = useState(transaction?.notes || '');
   const [accounts, setAccounts] = useState<Account[]>([]);
-  const [categories, setCategories] = useState<Category[]>([]);
+  const [expenseCategories, setExpenseCategories] = useState<Category[]>([]);
+  const [incomeCategories, setIncomeCategories] = useState<Category[]>([]);
+  const [targetAccount, setTargetAccount] = useState(transaction?.targetAccountId || '');
+  const [targetAmount, setTargetAmount] = useState(transaction?.targetAmount || 0);
 
   const router = useRouter();
+
   useEffect(() => {
     fetchAccounts().then(setAccounts);
-    fetchCategories().then(setCategories);
+    fetchCategories().then(({ expenseCategories, incomeCategories }) => {
+      setExpenseCategories(expenseCategories);
+      setIncomeCategories(incomeCategories);
+    });
   }, []);
 
   const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -90,13 +110,15 @@ export const TransactionForm = ({ locale, closeForm, transaction }: TransactionF
     const transactionRequest: TransactionRequest = {
       id: transaction?.id || undefined,
       accountId: Number(account),
+      targetAccountId: transactionType === 'transfer' ? Number(targetAccount) : null,
       label,
       amount,
-      categoryId: Number(category),
+      targetAmount: transactionType === 'transfer' ? Number(targetAmount) : null,
+      categoryId: transactionType !== 'transfer' ? Number(category) : null,
       dateTime: DateTime.fromISO(date + 'T' + time).toFormat("yyyy-MM-dd'T'HH:mm:ss.SSS"),
       notes,
-      isTransfer: false,
-      isIncome: false,
+      isTransfer: transactionType === 'transfer',
+      isIncome: transactionType === 'income',
     };
 
     const response = await submitTransaction(transactionRequest, transaction?.id ? false : true);
@@ -106,8 +128,11 @@ export const TransactionForm = ({ locale, closeForm, transaction }: TransactionF
     }
   };
 
-  const handlePopupClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    event.stopPropagation();
+  const handleTransactionTypeChange = (type: string) => {
+    setTransactionType(type);
+    if (type === 'transfer') {
+      setCategory('');
+    }
   };
 
   return (
@@ -121,15 +146,36 @@ export const TransactionForm = ({ locale, closeForm, transaction }: TransactionF
           backgroundColor: "var(--background)",
           color: "var(--foreground)",
         }}
-        onClick={handlePopupClick}
       >
+        {/* Close button */}
         <button
           className="absolute top-2 right-2 text-black hover:text-gray-800 cursor-pointer text-2xl border-black bg-gray-300"
           onClick={() => closeForm()}
         >
           ✖
         </button>
+
         <form onSubmit={handleFormSubmit} className="space-y-4">
+          {/* Transaction Type Switcher */}
+          <div className="flex justify-around mb-4">
+            <button type="button"
+              className={`px-4 py-2 rounded-md ${transactionType === 'expense' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+              onClick={() => handleTransactionTypeChange('expense')} >
+              {t('expense')}
+            </button>
+            <button type="button"
+              className={`px-4 py-2 rounded-md ${transactionType === 'income' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+              onClick={() => handleTransactionTypeChange('income')} >
+              {t('income')}
+            </button>
+            <button type="button"
+              className={`px-4 py-2 rounded-md ${transactionType === 'transfer' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+              onClick={() => handleTransactionTypeChange('transfer')} >
+              {t('transfer')}
+            </button>
+          </div>
+
+          {/* Label */}
           <div>
             <label htmlFor="label" className="block text-sm font-medium">
               {t('label')}
@@ -146,6 +192,7 @@ export const TransactionForm = ({ locale, closeForm, transaction }: TransactionF
             />
           </div>
 
+          {/* Account */}
           <div>
             <label htmlFor="account" className="block text-sm font-medium">
               {t('account')}
@@ -160,11 +207,14 @@ export const TransactionForm = ({ locale, closeForm, transaction }: TransactionF
             >
               <option value="">{t('selectAccount')}</option>
               {accounts.map((account) => (
-                <option key={account.id} value={account.id}>{account.name}</option>
+                <option key={account.id} value={account.id}>
+                  {account.name}
+                </option>
               ))}
             </select>
           </div>
 
+          {/* Amount */}
           <div>
             <label htmlFor="amount" className="block text-sm font-medium">
               {t('amount')}
@@ -181,26 +231,76 @@ export const TransactionForm = ({ locale, closeForm, transaction }: TransactionF
             />
           </div>
 
-          <div>
-            <label htmlFor="category" className="block text-sm font-medium">
-              {t('category')}
-            </label>
-            <select
-              id="category"
-              name="category"
-              className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
-              required
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-            >
-              <option value="">{t('selectCategory')}</option>
-              {categories.map((category) => (
-                <option key={category.id} value={category.id}>{category.name}</option>
-              ))}
-            </select>
-          </div>
+          {transactionType === 'transfer' && (
+            <>
+              {/* Target Account */}
+              <div>
+                <label htmlFor="targetAccount" className="block text-sm font-medium">
+                  {t('targetAccount')}
+                </label>
+                <select
+                  id="targetAccount"
+                  name="targetAccount"
+                  className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+                  required
+                  value={targetAccount}
+                  onChange={(e) => setTargetAccount(e.target.value)}
+                >
+                  <option value="">{t('selectTargetAccount')}</option>
+                  {accounts.map((account) => (
+                    <option key={account.id} value={account.id}>
+                      {account.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
 
+              {/* Target Amount */}
+              <div>
+                <label htmlFor="targetAmount" className="block text-sm font-medium">
+                  {t('targetAmount')}
+                </label>
+                <input
+                  type="number"
+                  id="targetAmount"
+                  name="targetAmount"
+                  className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+                  placeholder={t('enterTargetAmount')}
+                  required
+                  value={targetAmount}
+                  onChange={(e) => setTargetAmount(Number(e.target.value))}
+                />
+              </div>
+            </>
+          )}
+
+          {/* Category */}
+          {transactionType !== 'transfer' && (
+            <div>
+              <label htmlFor="category" className="block text-sm font-medium">
+                {t('category')}
+              </label>
+              <select
+                id="category"
+                name="category"
+                className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+                required
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option value="">{t('selectCategory')}</option>
+                {(transactionType === 'expense' ? expenseCategories : incomeCategories).map(category => (
+                  <option key={category.id} value={category.id}>
+                    {category.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
+
+          {/* Date and time */}
           <div className="flex space-x-4">
+            {/* Date */}
             <div className="flex-1">
               <label htmlFor="date" className="block text-sm font-medium">
                 {t('date')}
@@ -215,6 +315,8 @@ export const TransactionForm = ({ locale, closeForm, transaction }: TransactionF
                 onChange={(e) => setDate(e.target.value)}
               />
             </div>
+
+            {/* Time */}
             <div className="flex-1">
               <label htmlFor="time" className="block text-sm font-medium">
                 {t('time')}
@@ -231,6 +333,7 @@ export const TransactionForm = ({ locale, closeForm, transaction }: TransactionF
             </div>
           </div>
 
+          {/* Notes */}
           <div>
             <label htmlFor="notes" className="block text-sm font-medium">
               {t('notes')}
@@ -246,6 +349,7 @@ export const TransactionForm = ({ locale, closeForm, transaction }: TransactionF
             ></textarea>
           </div>
 
+          {/* Submit button */}
           <div>
             <button
               type="submit"
