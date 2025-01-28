bd22623fe04bde9bb59bb0a3f81b38a17e59dea8
Yuriy Peskov <yuriy.peskov@gmail.com>
2025-01-28 10:11:25 +0200

    refactoring, after logout redirect to login page

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -1,11 +1,10 @@
 import LoginPage from "@/components/login/page";
 
-interface FormData {
-  email: string;
-  password: string;
+type LoginPageParams = {
+  params: Promise<{ locale: string }>
 }
 
-export default async function LoginServerPage({ params }: { params: Promise<{ locale: string }> }) {
+export default async function LoginServerPage({ params }: LoginPageParams) {
   const { locale } = await params;
 
   return <LoginPage locale={locale} />;
