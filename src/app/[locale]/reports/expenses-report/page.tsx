041c229ef5436878b2d84d894dea3e1bc22a4dc9
Yuriy Peskov <yuriy.peskov@gmail.com>
2025-01-29 09:25:09 +0200

    add centralized place for processing API errors

--- src/app/[locale]/reports/expenses-report/page.tsx
+++ src/app/[locale]/reports/expenses-report/page.tsx
@@ -5,60 +5,53 @@ import AggregatedExpenses from "@/components/reports/expenses/AggregatedExpenses
 import DatePicker from "@/components/reports/expenses/DatePicker";
 import CategoriesExpenses from "@/components/reports/expenses/CategoriesExpenses";
 import EmptyCategoriesChecker from "@/components/reports/expenses/EmptyCategoriesChecker";
+import { request } from "@/utils/request/api";
+import { redirect } from "next/navigation";
+import routes from "@/routes/routes";
+import { UnauthorizedError } from "@/utils/request/errors";
 
-
-interface ExpensesReportPageProps {
+type ExpensesReportPageProps = {
   params: Promise<Record<string, string | undefined>>;
   searchParams: Promise<Record<string, string | undefined>>;
+  locale: string;
 }
 
-const getExpenses = async (fromDate: string, toDate: string, hideEmptyCategories: boolean, authToken: string) => {
-  const response = await fetch(apiRoutes.expenses(), {
-    method: 'POST',
-    body: JSON.stringify({
-      startDate: fromDate,
-      endDate: toDate,
-      hideEmptyCategories: hideEmptyCategories,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-      'auth-token': authToken,
-    },
-  });
-
-  if (!response.ok) {
-    const error = await response.json();
-    for (const key in error.detail) {
-      console.error(error.detail[key]);
+const getExpenses = async (fromDate: string, toDate: string, hideEmptyCategories: boolean, locale: string, t: any) => {
+  try {
+    const expenses = await request(apiRoutes.expenses(), {
+      method: 'POST',
+      body: JSON.stringify({
+        startDate: fromDate,
+        endDate: toDate,
+        hideEmptyCategories: hideEmptyCategories,
+      }),
+    });
+    return expenses;
+  } catch (error) {
+    if (error instanceof UnauthorizedError) {
+      redirect(routes.login(locale));
     }
+    console.error(error);
     return null;
   }
-
-  return await response.json();
 }
 
 const getAggregatedExpenses = async (fromDate: string, toDate: string, hideEmptyCategories: boolean, authToken: string) => {
-  const response = await fetch(apiRoutes.expensesAggregate(), {
-    method: 'POST',
-    body: JSON.stringify({
-      startDate: fromDate,
-      endDate: toDate,
-      hideEmptyCategories: hideEmptyCategories,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-      'auth-token': authToken,
-    },
-  });
+  try {
+    const response = await request(apiRoutes.expensesAggregate(), {
+      method: 'POST',
+      body: JSON.stringify({
+        startDate: fromDate,
+        endDate: toDate,
+        hideEmptyCategories: hideEmptyCategories,
+      }),
+    });
 
-  if (!response.ok) {
-    const error = await response.json();
-    for (const key in error.detail) {
-      console.error(error.detail[key]);
-    }
+    return response;
+  } catch (error) {
+    console.error(error);
     return null;
   }
-  return await response.json();
 }
 
 const getDiagramUrl = async (fromDate: string, toDate: string, authToken: string) => {
@@ -93,7 +86,7 @@ export default async function ExpensesReportPage({ params, searchParams }: Expen
   }
 
   const [responseExpenses, responseAggregated, responseDiagram] = await Promise.all([
-    getExpenses(fromDateInitial, toDateInitial, hideEmptyCategoriesInitial, authToken),
+    getExpenses(fromDateInitial, toDateInitial, hideEmptyCategoriesInitial, locale, t),
     getAggregatedExpenses(fromDateInitial, toDateInitial, hideEmptyCategoriesInitial, authToken),
     getDiagramUrl(fromDateInitial, toDateInitial, authToken),
   ])
--- src/utils/request/api.ts
+++ src/utils/request/api.ts
@@ -1,5 +1,6 @@
 import { getAuthToken } from "@/utils/auth";
 import { request as apiRequest } from "@/utils/request/fetch";
+import { UnauthorizedError } from "./errors";
 
 export async function request(url: string, options: RequestInit) {
   const token = await getAuthToken();
@@ -8,7 +9,14 @@ export async function request(url: string, options: RequestInit) {
     ...(token ? { "auth-token": token } : {}),
   };
 
-  const response = await apiRequest(url, { ...options, headers });
-
-  return await response.json();
+  try {
+    const response = await apiRequest(url, { ...options, headers });
+    return await response.json();
+  } catch (error) {
+    if (error instanceof UnauthorizedError) {
+      console.log('Unauthorized');
+      throw error;
+    }
+    throw new Error('Unknown error', { cause: error });
+  }
 }
